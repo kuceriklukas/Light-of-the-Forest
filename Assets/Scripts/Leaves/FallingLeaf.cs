55b5c29a05ed847a17b3df99f9f031114dcaefaa
michalderdak <michalderdak@gmail.com>
2016-04-04 10:48:08 +0200

    leaves

---
+++ Assets/Scripts/Leaves/FallingLeaf.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Leaves
+{
+    public class FallingLeaf : MonoBehaviour
+    {
+
+        public float Speed;
+
+        private float _screenBottom;
+        private float _repeatTime = 1f;
+        private float _xSpeed = 0.02f;
+
+        // Use this for initialization
+        void Start()
+        {
+            _screenBottom = Camera.main.ViewportToWorldPoint(Vector3.zero).y;
+            InvokeRepeating("Leafing", _repeatTime, _repeatTime);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            transform.position -= new Vector3(_xSpeed, Speed, 0);
+            if (transform.position.y < _screenBottom - 0.5f)
+            {
+                gameObject.SetActive(false);
+                SpawningLeaves.leavesPool.Enqueue(gameObject);
+            }
+        }
+
+        public void Leafing()
+        {
+            _xSpeed = Random.Range (-0.02f, 0.02f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Leaves/SpawningLeaves.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Leaves
+{
+    public class SpawningLeaves : MonoBehaviour
+    {
+
+        public static Queue<GameObject> leavesPool = new Queue<GameObject>();
+
+        public GameObject[] Objects;
+        public float SpawnTime = 2f;
+        public float FromPosition;
+        public float ToPosition;
+
+        private Vector3 spawnPosition;
+        private GameObject parent;
+
+        // Use this for initialization
+        void Start()
+        {
+            parent = GameObject.FindGameObjectWithTag("Leaf");
+            InvokeRepeating("Spawn", SpawnTime, SpawnTime);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Spawn()
+        {
+            spawnPosition.x = Random.Range(FromPosition, ToPosition);
+            spawnPosition.y = 13;
+
+            if (leavesPool.Count > 0)
+            {
+                GameObject leaf = leavesPool.Dequeue();
+                leaf.SetActive(true);
+                leaf.transform.position = spawnPosition;
+            }
+            else
+            {
+                GameObject e =
+                    Instantiate(Objects[Random.Range(0, Objects.Length - 1)], spawnPosition, Quaternion.identity) as
+                        GameObject;
+                e.transform.parent = parent.transform;
+            }
+        }
+    }
+}
