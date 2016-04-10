69031fbe18ff58f8fadbcb2bb93b524cf8607e33
michalderdak <michalderdak@gmail.com>
2016-04-10 16:18:21 +0200

    Revert "Collectible Script, Collectible Display (it follows camera)"
    This reverts commit 8c2079e79e75fd31e5defbcbeb0512f0d1245a02.

--- Assets/Scripts/Leaves/FallingLeaf.cs
+++ Assets/Scripts/Leaves/FallingLeaf.cs
@@ -11,13 +11,15 @@ namespace Assets.Scripts.Leaves
         public float YAxis;
 
         private float _screenBottom;
-        private Vector3 _spawnPositon;
+        private float _repeatTime = 1f;
+        private float _xSpeed = 0.02f;
+        private Vector3 spawnPositon;
         
 
         // Use this for initialization
         void Start()
         {
-            _spawnPositon = new Vector3(this.gameObject.transform.position.x,YAxis,0);
+            spawnPositon = new Vector3(this.gameObject.transform.position.x,YAxis,0);
             _screenBottom = Camera.main.ViewportToWorldPoint(Vector3.zero).y;
         }
 
@@ -28,7 +30,7 @@ namespace Assets.Scripts.Leaves
             transform.position -= new Vector3(0, Speed, 0);
             if (transform.position.y < _screenBottom - 0.5f)
             {
-                this.gameObject.transform.position = _spawnPositon;
+                this.gameObject.transform.position = spawnPositon;
             }
         }
         
