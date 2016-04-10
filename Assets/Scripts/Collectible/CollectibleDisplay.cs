5c53a963bb1e820567ae30ded893cc6a1054b0dd
michalderdak <michalderdak@gmail.com>
2016-04-10 17:47:45 +0200

    Collectibles, CollectibleCount

---
+++ Assets/Scripts/Collectible/CollectibleDisplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleDisplay : MonoBehaviour
+{
+
+    public float HowManyCollectibles;
+    public float XOffSet;
+    public float YOffSet;
+    public static int CollectibleCount;
+
+    private GameObject _camera;
+    private Vector3 _cameraPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    CollectibleCount = 0;
+
+        _camera = GameObject.FindGameObjectWithTag("MainCamera");
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    this.gameObject.GetComponent<TextMesh>().text = CollectibleCount + "/" + HowManyCollectibles;
+        _cameraPosition = _camera.transform.position;
+        this.gameObject.transform.position = new Vector3(_cameraPosition.x+XOffSet,_cameraPosition.y+YOffSet,-1);
+	}
+}
